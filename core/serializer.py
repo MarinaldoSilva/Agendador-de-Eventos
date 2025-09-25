33d3a07dba9fda7815bf4b2e08dd0d61d247d21b
Mario <marinaldo12@hotmail.com>
2025-09-25 00:02:51 -0300

    update evento serializer class

--- core/serializer.py
+++ core/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Evento
 
 
-class TarefaSerializer(serializers.ModelSerializer):
+class EventoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evento
         fields = ['id','nome_evento', 'data_evento', 'local_evento', 'data_criacao']
