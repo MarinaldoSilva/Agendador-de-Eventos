6113d13c8082c29bef9377cb17428e2086aad336
Mario <marinaldo12@hotmail.com>
2025-09-25 01:06:00 -0300

    regra de negocio da data de agendamento feita, crud feito

--- core/service.py
+++ core/service.py
@@ -1,8 +1,22 @@
+from django.utils import timezone
 from .models import Evento
 from .serializer import EventoSerializer
 
 
-class ServiceEvento:
+class EventoService:
+
+    @staticmethod
+    def _validar_data_evento(serializer):
+        """serializer.validated_data.get('key'), só é poss[ivel de verificação após o uso do serializer.is_valid():{...}"""
+        data_evento = serializer.validated_data.get('data_evento')
+        if data_evento and data_evento < timezone.now():
+            #se data_evento false, já vai para o return externo e não valida.
+            """A comparação das datas é feita após o serializer.is_valid(), se os dados passerem na validação, é feito a checagem das datas, após o is_valid() o campo data_evento é transformado em um obj do tipo data para salvar no banco e o datatime.now() é um obj, por isso quando fazemos a comparação do data_evento com o timezone.now() é feita a compração de dois objs do mesmo tipo yyyy-mm-ddThh:mm:ss..."""
+            return None
+        
+        """esse retorno 'None' é feito para que na validação na chamada da função create/update tenha algum retorno de passar pela validação"""
+        return None
+                
 
     @staticmethod
     def get_all_eventos():
@@ -14,11 +28,15 @@ class ServiceEvento:
     def create_evento(data):
         serializer = EventoSerializer(data=data)
         if serializer.is_valid():
+            erro_data_evento = EventoService._validar_data_evento(serializer)
+            if erro_data_evento:
+                return None, erro_data_evento
             serializer.save()
             return serializer.data, None
         return None, serializer.errors
     
 
+    """viewDetail in the as.view()"""
     @staticmethod
     def get_pk_evento(pk):
         try:
@@ -38,6 +56,9 @@ class ServiceEvento:
         
         serializer = EventoSerializer(instance=evento, data=data, partial = partial)
         if serializer.is_valid():
+            erro_data_evento = EventoService._validar_data_evento(serializer)
+            if erro_data_evento:
+                return None, erro_data_evento
             serializer.save()
             return serializer.data, None
         return None, serializer.errors
